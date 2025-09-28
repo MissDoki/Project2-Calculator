d6e983bf25c3b093af7d12ce00496ce614948541
Dokisan <49057924+MissDoki@users.noreply.github.com>
2025-09-28 08:59:24 +0200

    last update to the calculator
    this handles % module, fixed potential errors with wrong user input and much more

--- src/project2/calculator.java
+++ src/project2/calculator.java
@@ -1,58 +1,121 @@
 package project2;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class calculator {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		System.out.println(" ================================================================================");
+		System.out.println("|  WELCOME TO MATH CALCULATOR 1.0                                                |");
+		System.out.println("|                                                                                |");
+		System.out.println("|  You can use this program to calculate different types of basic                |");
+		System.out.println("|  calculations trough the menu.                                                 |");
+		System.out.println("|                                                                                |");
+		System.out.println("|  Use the input in the menu to run the program.                                 |");
+		System.out.println("|  To stop the program just input 'stop' in the console..                        |");
+		System.out.println("==================================================================================");
+		System.out.println("==================================================================================");
+		System.out.println("| Different type of operators you can use are:                                   |");
+		System.out.println("|                                                                                |");
+		System.out.println("| + Addition | - Subtraction | * Multiplication | / Division | % Division rest   |");
+		System.out.println("==================================================================================\n");
 		while (true) {
 			char operator;
-			double number1, number2, result;
-
-			System.out.println(" ================================================================================");
-			System.out.println("|  WELCOME TO MATH CALCULATOR 1.0                                                |");
-			System.out.println("|                                                                                |");
-			System.out.println("|  You can use this program to calculate different types of basic                |");
-			System.out.println("|  calculations trough the menu.                                                 |");
-			System.out.println("|                                                                                |");
-			System.out.println("|  Use the input in the menu to run the program.                                 |");
-			System.out.println("|  To stop the program just input 'stop' in the console..                        |");
-			System.out.println("==================================================================================");
-			System.out.println("==================================================================================");
-			System.out.println("| Different type of operators you can use are:                                   |");
-			System.out.println("|                                                                                |");
-			System.out.println("| + Addition | - Subtraction | * Multiplication | / Division                     |");
-			System.out.println("==================================================================================\n");
-			
+			double number, result;
+			boolean wrongInput = false;
 			System.out.print("Select an operator or type stop: ");
 			String option = input.nextLine();
-			if (option.equalsIgnoreCase("stop") ) {
+			if (option.equalsIgnoreCase("stop")) {
 				break;
 			}
-			
-			operator = option.charAt(0);
-			System.out.print("Input a number: ");
-			number1 = input.nextDouble();
-			System.out.print("Input another number: ");
-			number2 = input.nextDouble();
-			input.nextLine();
-			
+			try {
+				operator = option.charAt(0);
+			} catch (Exception e) {
+				continue;
+			}
 			switch (operator) {
 			case '+':
-				result = number1 + number2;
-				System.out.println("Total is: " + number1 + " + " + number2 + " = " + result + "\n");
+			case '-':
+			case '/':
+			case '*':
+			case '%':
+				break;
+			default:
+				System.out.println("\nInvalid input.\n" + "Enter a valid operator or 'stop' to close.\n"
+						+ "Operators to use are ( +, -, *, /, % )\n");
+				continue;
+			}
+			ArrayList<Double> numbers = new ArrayList<Double>();
+			for (int i = 0; i < 99;) {
+				wrongInput = true;
+				while (wrongInput) {
+					System.out.println("Input number or 'calc' for calculation: ");
+					try {
+						String userInput = input.nextLine();
+						if (userInput.equalsIgnoreCase("calc")) {
+							if (numbers.size() >= 2) {
+								i = 100;
+								break;
+							}
+						}
+						number = Double.parseDouble(userInput);
+						numbers.add(number);
+					} catch (Exception e) {
+						System.out.println("Invalid input. Input a valid number.\n");
+					}
+				}
+			}
+			switch (operator) {
+			case '+':
+				result = numbers.get(0);
+				System.out.print("Total is: " + result);
+
+				for (int i = 1; i < numbers.size(); i++) {
+					result += numbers.get(i);
+					System.out.print(" + " + numbers.get(i));
+				}
+				System.out.println(" = " + result + "\n");
 				break;
 			case '-':
-				result = number1 - number2;
-				System.out.println("Total is: " + number1 + " - " + number2 + " = " + result + "\n");
+				result = numbers.get(0);
+				System.out.print("Total is: " + result);
+
+				for (int i = 1; i < numbers.size(); i++) {
+					result -= numbers.get(i);
+					System.out.print(" - " + numbers.get(i));
+				}
+				System.out.println(" = " + result + "\n");
 				break;
 			case '*':
-				result = number1 * number2;
-				System.out.println("Total is: " + number1 + " * " + number2 + " = " + result + "\n");
+				result = numbers.get(0);
+				System.out.print("Total is: " + result);
+
+				for (int i = 1; i < numbers.size(); i++) {
+					result *= numbers.get(i);
+					System.out.print(" * " + numbers.get(i));
+				}
+				System.out.println(" = " + result + "\n");
 				break;
 			case '/':
-				result = number1 / number2;
-				System.out.println("Total is: " + number1 + " / " + number2 + " = " + result + "\n");
+				result = numbers.get(0);
+				System.out.print("Total is: " + result);
+
+				for (int i = 1; i < numbers.size(); i++) {
+					result /= numbers.get(i);
+					System.out.print(" / " + numbers.get(i));
+				}
+				System.out.println(" = " + result + "\n");
+				break;
+			case '%':
+				result = numbers.get(0);
+				System.out.print("Total is: " + result);
+
+				for (int i = 1; i < numbers.size(); i++) {
+					result %= numbers.get(i);
+					System.out.print(" % " + numbers.get(i));
+				}
+				System.out.println(" = " + result + "\n");
 				break;
 			default:
 				System.out.println("Invalid input.");
@@ -62,40 +125,3 @@ public class calculator {
 		input.close();
 	}
 }
-
-// Notes:
-//-> while loop * done
-//-> able to 'stop' * done
-//-> push 1 (empty file) * done
-//-> basic project (push 2) * done
-//-> print summation * done
-//(ask for operator > ask for number > ask for another number) * done
-//-> basic project + extra number input (push 3)
-//-> basic project + extra module input (push 4)
-//-> gui graphic module (branch 1)
-//-> clear calculation or continue - (extra after turn in)
-
-//Skapa ett javaprojekt enligt nedanstående kravspecifikation och gör minst
-//fyra push till repot. En per feature. Skapa en ny branch för respektive
-//feature. Ta inte bort brancher, du ska vid redovisningen kunna visa att du
-//arbetat enligt instruktionerna
-//
-//
-//
-//Kravspecifikation: Wigellkoncernen vill att du ska bygga en enkel kalkylator.
-//G Kalkylatorn ska ha funktionalitet för de fyra räknesätten + - * /Användaren
-//ska nå funktionaliteten för valt räknesätt genom en huvudmeny som byggs av en
-//switch-sats.Programmet ska köras kontinuerligt tills användaren väljer att
-//avsluta via ett val i menyn.Kalkylatorn behöver endast ha funktionalitet för
-//att hantera två tal i respektive beräkning
-//
-//Input och output sker via konsolen, användargränssnittet är
-//tillfredsställande
-//
-//Tomas Wigell ska bjudas in till projektet som collaboratorInlämning sker via
-//länk till githubrepo Mindre kompletteringar får förekomma
-//
-//VG Samtliga krav för G är uppfyllda Kalkylatorn kan hantera fler än 2 tal i
-//respektive beräkningFunktionalitet för operatorn % (modulus) ska
-//finnasAnvändargränssnittet visar på djup förståelse av den aspekten Uppgiften
-//ska vara inlämnad innan deadlineMindre kompletteringar får förekomma
